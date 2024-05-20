83a9f63ac076c33ed447722a5943c9adb7b06fd4
wajiha <w5428481@gmail.com>
2024-05-20 14:39:50 +0500

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Quizsreen from './components/Quizsreen';
+
+function App() {
+  return (
+   <div>
+   <Quizsreen/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Quizsreen.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Quizsreen = () => {
+  return (
+    <div className='container bg-info px-2'>
+      <div><img src="" alt="" /></div>
+      <div className='row'>
+        <div className='col'>
+        <h1>QuizApp</h1>
+        <h5>question</h5>
+        </div>
+      <div className='col'>
+     <button className=''>option</button>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Quizsreen
