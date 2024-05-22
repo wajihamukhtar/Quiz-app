65c347b1da87e69f32de87a90d9beeca09583a01
wajiha <w5428481@gmail.com>
2024-05-23 01:44:12 +0500

    update functionallity

--- src/components/Quizsreen.jsx
+++ src/components/Quizsreen.jsx
@@ -3,7 +3,11 @@ import Result from './Result';
 
 
 const Quizsreen = () => {
-  const[CurrentIndex,setCurrentIndex]=useState(0)
+
+  const [selectedOption, setSelectedOption] = useState([]);
+  const [CurrentIndex, setCurrentIndex] = useState(0)
+  const [marks, setMarks] = useState(0)
+  const [des, setDes] = useState(false)
 
   const quizData = [
     {
@@ -21,70 +25,89 @@ const Quizsreen = () => {
       options: ["color", "background-color", "bgcolor", "bg-color"],
       correctAnswer: "background-color"
     },
-    // {
-    //   question: "Which CSS property controls the text size?",
-    //   options: ["font-style", "text-size", "font-size", "text-style"],
-    //   correctAnswer: "font-size"
-    // },
-    // {
-    //   question: "What does CSS stand for?",
-    //   options: ["Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets"],
-    //   correctAnswer: "Cascading Style Sheets"
-    // },
-    // {
-    //   question: "Inside which HTML element do we put the JavaScript?",
-    //   options: ["<script>", "<js>", "<javascript>", "<code>"],
-    //   correctAnswer: "<script>"
-    // },
-    // {
-    //   question: "How do you write 'Hello World' in an alert box?",
-    //   options: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
-    //   correctAnswer: "alert('Hello World');"
-    // },
-    // {
-    //   question: "How do you create a function in JavaScript?",
-    //   options: ["function myFunction()", "function:myFunction()", "function = myFunction()", "function => myFunction()"],
-    //   correctAnswer: "function myFunction()"
-    // },
-    // {
-    //   question: "How do you call a function named 'myFunction'?",
-    //   options: ["call myFunction()", "call function myFunction()", "myFunction()", "run myFunction()"],
-    //   correctAnswer: "myFunction()"
-    // }
+    {
+      question: "Which CSS property controls the text size?",
+      options: ["font-style", "text-size", "font-size", "text-style"],
+      correctAnswer: "font-size"
+    },
+    {
+      question: "What does CSS stand for?",
+      options: ["Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets"],
+      correctAnswer: "Cascading Style Sheets"
+    },
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      options: ["<script>", "<js>", "<javascript>", "<code>"],
+      correctAnswer: "<script>"
+    },
+    {
+      question: "How do you write 'Hello World' in an alert box?",
+      options: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
+      correctAnswer: "alert('Hello World');"
+    },
+    {
+      question: "How do you create a function in JavaScript?",
+      options: ["function myFunction()", "function:myFunction()", "function = myFunction()", "function => myFunction()"],
+      correctAnswer: "function myFunction()"
+    },
   ];
-  return (
-    <div className='container-lg '>
-      {CurrentIndex < 3 ?(
 
-      <div className='row m-auto pb-12 mt-20 w-9/12 border rounded'>
-        <div className='col pl-5'>
-        <h1 className='text-center text-4xl pt-5 py-3'>QuizApp</h1>
-        <p>Question {CurrentIndex}/{quizData.length}</p>
-        <h5 className='text-2xl py-3'>
-        {quizData[CurrentIndex].question}
-        </h5>
-      <div className='col w-2/3'>
-      {quizData[CurrentIndex].options.map((option,i)=>(
-     <button className='border rounded w-72 py-1 mx-1 mt-5 text-xl bg-blue-300'
-     onClick={()=>{
-      if ( quizData[CurrentIndex].correctAnswer=== option) {
-        setCurrentIndex(CurrentIndex+1)
-      }
-      else if (quizData[CurrentIndex].correctAnswer!== option){
-       setCurrentIndex()
-      }
-     }}
-     >
-      {option}
-     </button>
-      ))}
-      </div>
-      </div>
-    </div>
-      ):( <Result text="Result" CurrentIndex={CurrentIndex}/>)
+  const handleOptionClick = (option, correctAnsIndex) => {
+    setDes(true)
+    setSelectedOption([...selectedOption, option])
+    const correctAns = quizData[correctAnsIndex]?.correctAnswer
+    if (correctAns === option) {
+      setMarks(marks + 1)
     }
-  </div>
-  )
-}
+    else {
+      return null
+    }
+  };
+  console.log(selectedOption)
+  const handleNextClick = () => {
+    setDes(false)
+    setCurrentIndex(CurrentIndex + 1)
+  };
 
-export default Quizsreen
+  return (
+    <div className='container-lg'>
+      {CurrentIndex < quizData.length ? (
+        <div className='row mx-auto w-96 pb-12 mt-24  border rounded'>
+          <div className='col px-5'>
+            <h1 className='text-center text-4xl pt-5 py-3'>QuizApp</h1>
+            <p>Questions: {CurrentIndex + 1}/{quizData.length}</p>
+            <h5 className='text-2xl py-3'>
+              {quizData[CurrentIndex]?.question}
+            </h5>
+            <div class="">
+              {quizData[CurrentIndex]?.options?.map((option, index) => (
+                <div className=''>
+                  <button
+                    key={index}
+                    disabled={des}
+                    className={`border rounded  py-1 mt-5 text-xl w-full  ${selectedOption?.includes(option) ? 'bg-green-300' : des ? 'bg-slate-200 text-slate-500' : 'bg-blue-200'}`}
+                    onClick={() => handleOptionClick(option, CurrentIndex)}
+                  >
+                    {option}
+                  </button>
+                </div>
+              ))}
+              <button
+                disabled={!des}
+                onClick={handleNextClick}
+                className={`border rounded px-4 py-1 mt-5 text-xl ${des ? 'bg-blue-200' :'bg-slate-300 text-slate-500'}`}
+              >
+                {CurrentIndex === 7 ? 'Submit' : 'Next'}
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Result text="Result" marks={marks} selectedOption={selectedOption} />
+      )
+      }
+    </div >
+  );
+};
+
+export default Quizsreen;
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Result = (props) => {
-    const {text,CurrentIndex}=props
+  const { text, marks, selectedOption } = props
+  const [showAns, setShowAns] = useState(false)
+
   return (
-    <div {...props}>
-      <h1 >{text}</h1>
-      <p>{CurrentIndex} of 40</p>
-      <button>Chack your Answer</button>
-      <button >Try Again</button>
+    <div className='bg-blue-200 mx-auto w-96 pb-12 mt-24 text-center px-8  border rounded' {...props}>
+      <h1 className='text-5xl py-3'>{text}</h1>
+      <p className='text-2xl'>{marks} / 8</p>
+      <button className='border rounded  py-1 mt-5 text-lg w-52 bg-black text-blue-200' onClick={() => setShowAns(true)} >Chack your Answer</button>
+      <br />
+      <button className='border rounded  py-1 mt-5 text-lg w-52  bg-black text-blue-200' onClick={() => window.location.href = '/'}>Try Again</button>
+      <ul>
+      {showAns && selectedOption?.map((ans, index) => (
+       <li  className='list-decimal mt-2 pl-1 text-left'>
+        <p key={index}>{ans}</p>
+       </li>))}
+       </ul>
     </div>
   )
 }
